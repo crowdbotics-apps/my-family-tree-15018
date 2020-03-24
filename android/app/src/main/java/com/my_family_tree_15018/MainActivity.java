5f7afda32eaaa7b03241bfeca5c4422dd81af949
Crowdbotics <team@crowdbotics.com>
2020-03-24 13:54:22 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/my_family_tree_15018/MainActivity.java
@@ -0,0 +1,15 @@
+package com.my_family_tree_15018;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "my_family_tree_15018";
+    }
+}
